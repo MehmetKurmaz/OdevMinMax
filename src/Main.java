1156f4fe3982b1d41d19357ca37e2f85fce00cd6
MehmetKurmaz <111184509+MehmetKurmaz@users.noreply.github.com>
2023-08-01 19:27:20 +0300

    added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int miktar,input,max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
+        System.out.print("Giris yapacaginiz Sayi Miktarini Yaziniz :");
+        Scanner scanner=new Scanner(System.in);
+        miktar=scanner.nextInt();
+        for (int i=0;i<miktar;){
+            i++;
+            System.out.print(i+".Sayi Giriniz :");
+            input=scanner.nextInt();
+            if (input>max){
+                max=input;
+
+            } else if (input<min) {
+                min=input;
+
+            }
+
+        }
+        System.out.println("En Buyuk Deger :"+max);
+        System.out.println("En Kucuk Deger :"+min);
     }
 }
